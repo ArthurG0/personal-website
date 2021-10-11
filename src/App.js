e7ee13c59dd20aabfe06c96ea1f48ede8c9fb4e9
ArthurG0 <agatin@umail.ucsb.edu>
2021-10-11 00:22:03 -0700

    trying to get website

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
   return (
     <Router>
       <Switch>
-      <Route exact path="/" >
-        <Home empty/>
-      </Route>
+        <Route exact path="/" >
+          <Home/>
+        </Route>
 
-      <Route path='*' >
-        <Home error404/>
-      </Route>
+        <Route path='*' >
+          <Home/>
+        </Route>
       </Switch>
 
 
